ee7a14272c404efd435746ce088a7ee0124cc2b5
Pranav Tartey <pranavtartey@gmail.com>
2024-12-16 17:19:46 +0530

    added the logoticker

--- app/components/LogoTicker.tsx
+++ app/components/LogoTicker.tsx
@@ -4,6 +4,8 @@ import echoLogo from "../assets/images/echo.png";
 import celestialLogo from "../assets/images/celestial.png";
 import pulseLogo from "../assets/images/pulse.png";
 import apexLogo from "../assets/images/apex.png";
+import React from "react";
+import Image from "next/image";
 
 const images = [
   { src: acmeLogo, alt: "Acme Logo" },
@@ -16,8 +18,23 @@ const images = [
 
 export const LogoTicker = () => {
   return (
-    <section>
-      
+    <section className="bg-black text-white py-[72px] sm:py-24">
+      <div className="container">
+        <div className="mb-8">
+          <h2 className="text-center text-xl tracking-wide max-sm:max-w-[300px] mx-auto text-white/70">
+            Trusted by the world's most innovative teams
+          </h2>
+        </div>
+        <div className="[mask-image:linear-gradient(to_right,transparent,black,transparent)] overflow-x-hidden">
+          <div className="flex flex-none gap-14">
+            {images.map((img) => (
+              <React.Fragment key={img.alt}>
+                <Image src={img.src} alt={img.alt} />
+              </React.Fragment>
+            ))}
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
