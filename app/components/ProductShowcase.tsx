f4d39d466f4fc3f7e42c6061a2ccd0d2bb118afd
Pranav Tartey <pranavtartey@gmail.com>
2024-12-16 20:28:05 +0530

    added the products section

--- app/components/ProductShowcase.tsx
+++ app/components/ProductShowcase.tsx
@@ -1,5 +1,21 @@
+import Image from "next/image";
 import appScreen from "../assets/images/app-screen.png";
 
 export const ProductShowcase = () => {
-  return null;
+  return (
+    <section className="bg-black text-white bg-gradient-to-b from-black to-[#5D2CA8] py-[72px] sm:py-24">
+      <div className="container">
+        <h2 className="text-center text-5xl sm:text-6xl font-bold tracking-tighter">
+          Intuitive Interface
+        </h2>
+        <p className="text-xl text-center text-white/70 mt-5 max-w-xl mx-auto">
+          Celebrate the joy of accomplishment with an app designed to track your
+          progress, motivate your efforts, and celebrate your successes, one
+          task at a time.
+        </p>
+
+        <Image src={appScreen} alt="app-screen" className="mt-14 mx-auto" />
+      </div>
+    </section>
+  );
 };
